92f35668e2e91824ac3af13189ab599fb28a31d8
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2023-05-03 23:35:24 +0600

    daisyUi added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
@@ -9,13 +8,10 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        
       </div>
+      
+
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -25,6 +21,36 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
+      <path
+        d="M100,20 L20,100 L100,180 L180,100 Z"
+        fill="#F86767"
+        stroke="#000"
+        strokeWidth="4"
+      />
+      <text
+        x="50%"
+        y="60%"
+        textAnchor="middle"
+        fill="#FFF"
+        fontFamily="Arial"
+        fontSize="72"
+        fontWeight="bold"
+      >
+        Mezban
+      </text>
+      <text
+        x="50%"
+        y="80%"
+        textAnchor="middle"
+        fill="#F86767"
+        fontFamily="Arial"
+        fontSize="24"
+      >
+        Restaurant &amp; Catering Services
+      </text>
+    </svg>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
